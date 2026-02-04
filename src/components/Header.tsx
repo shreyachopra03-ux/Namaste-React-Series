5410ee4688dff081694bb72934def5f4e0f90408
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-04 15:32:34 +0530

    Update: Added Login/Logout functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 // This is how we write a 'named import'
 import {LOGO_URL} from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+  const [btnNameReact, setbtnNameReact] = useState('Login');
+  
   return (
     <div className="header">
       <div className="logo-container">
@@ -13,6 +16,15 @@ const Header = () => {
           <li>About Us</li>
           <li>Cart</li>
           <li>Contact Us</li>
+          <button className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+              ? setbtnNameReact('Logout')
+              : setbtnNameReact('Login');
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
