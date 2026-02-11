8cb19f81a9f6ba2aa8768bbf5015ea45659881bf
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-11 21:55:45 +0530

    feat: implement dynamic open/closed status badge for restaurant cards

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,8 +1,9 @@
-import RestaurantCard from "./RestaurantCard";
+import RestaurantCard, { OpenOrNot } from "./RestaurantCard";
 import RestaurantData from "../utils/restaurantData.json";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import useOnlineStatus from "../utils/useOnlineStatus";
+import { Link } from "react-router-dom";
 
 type Restaurant = {
     info: {
@@ -11,6 +12,7 @@ type Restaurant = {
         cuisines: string[];
         avgRating: number;
         cloudinaryImageId: string;
+        isOpen: boolean;
         costForTwo: string;
         sla: {
             deliveryTime: number;
@@ -23,16 +25,16 @@ const Body = () => {
     // <Restaurant[]> => iska mtlb hai ki jo bhi value aayegi vo Restaurant type ke objects ka ARRAY hoga
     // ([]) => intial value mtlb starting mei koi restaurant nhi hai
 
-    const [listOfRestaurants, setlistOfRestaurants] = useState<Restaurant[]>(
-        []
-    );
-    const [filteredRestaurant, setFilteredRestaurant] = useState<Restaurant[]>(
-        [],
-    );
+    const [listOfRestaurants, setlistOfRestaurants] = useState<Restaurant[]>([]);
+
+    const [filteredRestaurant, setFilteredRestaurant] = useState<Restaurant[]>([]);
 
     const [searchText, setSearchText] = useState("");
 
+    const RestaurantCardAvailable = OpenOrNot(RestaurantCard);
+   
     // whenever state variable updates, react triggers a reconciliation cycle (re-renders the component)
+    console.log("Body Rendered", listOfRestaurants);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -75,7 +77,6 @@ const Body = () => {
           </h1>
     );
 
-
     // This is Conditional Rendering -> rendering on the basis of any condition
     if (listOfRestaurants.length === 0) {
         return <Shimmer />;
@@ -116,7 +117,7 @@ const Body = () => {
                     onClick={() => {
                         const filteredList = listOfRestaurants.filter(
                             (res) => res.info.avgRating > 4,
-                        );
+                         );
                         setlistOfRestaurants(filteredList);
                     }}
                 >
@@ -124,13 +125,19 @@ const Body = () => {
                 </button>
             </div>
 
-            <div className="res-container">
-                {filteredRestaurant.map((restaurant) => (
-                    <RestaurantCard
-                        key={restaurant.info.id}
-                        resData={restaurant}
-                    />
-                ))}
+            <div className="flex flex-wrap items-center space-between">
+                {filteredRestaurant.map((restaurant) => {
+                    return (
+                    <div key={restaurant.info.id}>
+                    {restaurant.info.isOpen ? (
+                        <RestaurantCardAvailable resData={restaurant} />
+                    ) : (
+                        <RestaurantCard resData={restaurant} />
+                    )}
+                    </div>
+
+                    );
+                    })}
             </div>
         </div>
     );
