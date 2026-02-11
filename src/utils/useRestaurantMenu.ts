b42d57252bf7d1565f643318d4fb48f7dbeeafa6
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-11 13:49:55 +0530

    updated the restaurant menu

--- src/utils/useRestaurantMenu.ts
+++ src/utils/useRestaurantMenu.ts
@@ -7,11 +7,13 @@ const useRestaurantMenu = (resId?: string) => {
 
     useEffect(() => {
         fetchData();
-    }, []);
+    }, [resId]);
 
     const fetchData = async () => {
         const data = await fetch(MENU_API + resId);
         const json = await data.json();
+
+        setResInfo(json.data);
     }
     return resInfo;
 
