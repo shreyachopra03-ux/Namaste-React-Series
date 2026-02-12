57f0e854cc745b646d323c318da0e9790fa81878
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-12 16:54:52 +0530

    Accordion functionality

--- src/components/RestaurantCategory.tsx
+++ src/components/RestaurantCategory.tsx
@@ -1,25 +1,34 @@
 import ListItem from "./ListItem";
+import { useState } from "react";
 
 type Props = {
     data: any;
 };
 
 const RestaurantCategory= ({ data }:Props) => {
-    console.log(data);
+
+    const [showItems, setShowItems] = useState(false);
+
+    const handleClick = () => {
+       
+        setShowItems(!showItems);
+    }
+  
     return (
          <div>
             {/* Header */}
         <div className="m-auto w-6/12 bg-gray-50 shadow-lg p-4 mx-auto my-4">
-        <div className="flex justify-between">
+        <div className="flex justify-between cursor-pointer" onClick={handleClick}>
             <span className="font-bold text-lg">{data?.title} ({data?.itemCards?.length})</span>
             <span>⬇️</span>
         </div>
             
             {/* Accordion Body */}
-            <ListItem items={data?.itemCards || []} />
+            {showItems && <ListItem items={data?.itemCards || []} />}
         </div>
          </div>
 
     );
 };
+
 export default RestaurantCategory;
