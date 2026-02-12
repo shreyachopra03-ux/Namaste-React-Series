954c317306767d2ab97cfeeeff61b0b194e9648f
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-12 15:17:59 +0530

    Accordion header & Body

--- src/components/RestaurantCategory.tsx
+++ src/components/RestaurantCategory.tsx
@@ -1,17 +1,23 @@
+import ListItem from "./ListItem";
+
 type Props = {
     data: any;
 };
 
 const RestaurantCategory= ({ data }:Props) => {
-    console.log(data)
+    console.log(data);
     return (
          <div>
             {/* Header */}
         <div className="m-auto w-6/12 bg-gray-50 shadow-lg p-4 mx-auto my-4">
-            <span>{data.title}</span>
+        <div className="flex justify-between">
+            <span className="font-bold text-lg">{data?.title} ({data?.itemCards?.length})</span>
             <span>⬇️</span>
         </div>
+            
             {/* Accordion Body */}
+            <ListItem items={data?.itemCards || []} />
+        </div>
          </div>
 
     );
