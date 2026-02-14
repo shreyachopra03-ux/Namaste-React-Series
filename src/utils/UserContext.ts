71d20d2c75d0517d640e329c50950ca6c971a132
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-14 23:26:34 +0530

    add typed UserContext with state setter

--- src/utils/UserContext.ts
+++ src/utils/UserContext.ts
@@ -1,8 +1,39 @@
 import { createContext } from "react";
 
-const UserContext = createContext({
+const UserContext = createContext<{
+    loggedInUser: string,
+    setUserName: React.Dispatch<React.SetStateAction<string>>;
+}>({
     loggedInUser: "Default User",
+    setUserName: () => {},
 });
-
 export default UserContext;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
