b3a6cf455502864e29649ca075e429cf5e861e47
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-15 23:23:51 +0530

    App Store

--- src/utils/appStore.tsx
+++ src/utils/appStore.tsx
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "../slices/cartSlice";
 
 const appStore = configureStore({
-    reducer: {}
+    // This is the big reducer which collects all the reducers from all the different slices 
+    reducer: {
+        cart: cartReducer
+    }
 });
 
 export default appStore;
