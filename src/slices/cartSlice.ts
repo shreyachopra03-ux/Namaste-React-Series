fc016ffacfe38a94363ba0ce6280a2e413cf4255
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-15 23:19:37 +0530

    cart slice

--- src/slices/cartSlice.ts
+++ src/slices/cartSlice.ts
@@ -3,9 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
     name: 'cart',
     initialState: {
+        // These items are cart-items & initially it's empty
         items: [],
     },
     // Reducer will always take 2 arguments: state & action (vvimp)
+    //Reducers are basically objects that define what type of actions they can take.
+    // action.payload is the daata that we send during dispatch (action).
+    // state.items means the items array inside the state, and action.payload is the data that gets added to this array.
+    
     reducers: {
         addItem: (state:any, action:any) => {
             state.items.push(action.payload);
