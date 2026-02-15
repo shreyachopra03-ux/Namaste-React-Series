076b6d450d64e09ba533b0a90ce1e1c0b1965bf5
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-16 02:31:54 +0530

    modified the cartSlice, added type & interface

--- src/slices/cartSlice.ts
+++ src/slices/cartSlice.ts
@@ -1,26 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+ 
+type CartItem = string;
+
+export interface CartState {
+    items: CartItem[];
+}
+
+const initialState: CartState = {
+    items: [],
+}
 
 const cartSlice = createSlice({
     name: 'cart',
-    initialState: {
+    initialState,
+    // {
         // These items are cart-items & initially it's empty
-        items: [],
-    },
+        // items: [],
+    // },
     // Reducer will always take 2 arguments: state & action (vvimp)
     //Reducers are basically objects that define what type of actions they can take.
     // action.payload is the daata that we send during dispatch (action).
     // state.items means the items array inside the state, and action.payload is the data that gets added to this array.
 
     reducers: {
-        addItem: (state:any, action:any) => {
-            // mutating the state here
+        addItem: (state:any, action: PayloadAction<CartItem>) => {
+            // mutating the state here -> means to change it directly
             state.items.push(action.payload);
         },
-        removeItem: (state:any, action:any) => {
+        removeItem: (state:any, action: PayloadAction<CartItem>) => {
             state.items.pop();
         },
-        clearCart: (state:any, action:any) => {
-            state.items.length = 0; // []
+        clearCart: (state:any, action: PayloadAction<CartItem>) => {
+            state.items = []
         },
     },
 });
