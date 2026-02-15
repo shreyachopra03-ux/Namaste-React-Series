ab1b817641f0fcf7ffdd30f21e0ca039b5e09bfd
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-15 23:10:16 +0530

    created cart slice

---
+++ src/slices/cartSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        items: [],
+    },
+    reducers: {
+        addItem: (state:any, action:any) => {
+            state.items.push(action.payload);
+        },
+        removeItem: (state:any, action:any) => {
+            state.items.pop();
+        },
+        clearCart: (state:any, action:any) => {
+            state.items.length = 0;
+        },
+    },
+});
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
