fb7947f550a6c659197d013692046a5ec74f5347
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-16 02:29:34 +0530

    update: modified the app store

--- src/utils/appStore.tsx
+++ src/utils/appStore.tsx
@@ -4,8 +4,11 @@ import cartReducer from "../slices/cartSlice";
 const appStore = configureStore({
     // This is the big reducer which collects all the reducers from all the different slices 
     reducer: {
-        cart: cartReducer
+        cart: cartReducer,
     }
 });
 
 export default appStore;
+
+// AppDispatch tell the exact type of dispatch funtion in redux to TS, so that invalid actions could be dispatched 
+export type AppDispatch = typeof appStore.dispatch;
