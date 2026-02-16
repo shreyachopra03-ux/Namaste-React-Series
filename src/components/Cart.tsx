390b4213284b77e5914c6ebd5de5d33bca48da63
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-16 19:00:20 +0530

    Added the clear cart functionality

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import ListItem from "./ListItem";
 import { useDispatch } from "react-redux";
 import { clearCart } from "../slices/cartSlice";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
     const cartItems = useSelector((store:any) => store.cart.items)
@@ -16,11 +17,22 @@ const Cart = () => {
     return (
         <div className="text-center m-4 p-4">
             <h1 className="text-2xl font-bold">Cart</h1>
+        
         <div className="w-6/12 m-auto">      
-            <button className="p-2 m-2 rounded-lg text-white bg-black"
+            <button className="p-2 m-2 rounded-md text-white bg-orange-600"
                onClick={handleClearCart}>
                 Clear Cart
-            </button><ListItem items={cartItems} />
+            </button>
+            {cartItems.length == 0 && (
+                <h1 className="font-mono m-7">Your cart is empty<br/>
+                You can go to the home page to view more restaurants❤️
+                </h1>
+            )} 
+            <button className="p-2 -my-4 rounded-md text-white bg-orange-600 cursor-pointer">
+                <Link to="/">SEE RESTAURANTS NEAR YOU</Link>
+            </button>
+            
+            <ListItem items={cartItems} />
         </div>
         </div>
     )
