3da8e63c090003652d2abcff2f175911818929da
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-20 00:47:46 +0530

    feat(ui): add text boxes and submit button

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -1,7 +1,20 @@
 const ContactUs = () => {
     return (
         <div>
-            <h1>Contact Us page</h1>
+            <h1 className="font-bold text-3xl p-4 m-4">Contact Us page</h1>
+            <form>
+                <input type="text" 
+                className="border border-black -2 p-2" 
+                placeholder="name"
+                />
+                <input type="text"
+                 className="border border-black -2 p-2" 
+                 placeholder="message" 
+                 />
+                <button className="border-black m-2 p-2 bg-gray-100 rounded-lg border">
+                Submit
+                </button>
+            </form>
         </div>
     )
 }
