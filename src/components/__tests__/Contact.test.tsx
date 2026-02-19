d1502b37d27580a9eb285195b6414d4f8304d450
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-20 00:47:00 +0530

    config file

--- src/components/__tests__/Contact.test.tsx
+++ src/components/__tests__/Contact.test.tsx
@@ -11,3 +11,38 @@ test("Should load contact us component", () => {
     expect(heading).toBeInTheDocument();
 });
 
+test("Should load Button inside contact component", () => {
+    render(<ContactUs/>);
+
+    // const button = screen.getByRole("button");
+
+    // Another way of doing the above thing
+    const button = screen.getByText('Submit')
+
+    // Assertion
+    expect(button).toBeInTheDocument();
+    
+});
+
+test("Should load input name inside contact component", () => {
+    render(<ContactUs/>);
+
+    const inputName = screen.getByPlaceholderText('name')
+
+    // Assertion
+    expect(inputName).toBeInTheDocument();
+    
+});
+
+test("Should load 2 input boxes on the Contact component", () => {
+    render(<ContactUs/>);
+
+    // Querying
+    // Whenever there are multiple items to be tested, we will use All 
+     const inputBoxes = screen.getAllByRole('textbox');
+
+     console.log(inputBoxes.length);
+
+     // Assertion
+     expect(inputBoxes.length).toBe(2);
+});
