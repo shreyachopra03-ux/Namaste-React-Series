24d7e78812c3b1cf57b46b094bd77453410c2f5a
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-17 23:04:56 +0530

    style: improve styling & made layout responsive

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -28,16 +28,25 @@ const RestaurantCard = (props: RestaurantCardProps) => {
   const { loggedInUser } = useContext(UserContext);
   return (
     <Link to={`/restaurants/${id}`} className="restaurant-card-link">
-      <div data-testid="resCard" className="m-4 p-4 w-62.5 bg-orange-100 hover:bg-orange-200 rounded-lg">
-        <img className="rounded-lg" alt="res-logo" src={CDN_URL + info.cloudinaryImageId}/>
-        <h4 className="font-bold py-4 text-lg">{info.name}</h4>
-        <h4>{info.cuisines.join(", ")}</h4>
-        <h4>{info.avgRating}⭐</h4>
-        <h4>{info.costForTwo}</h4>
-        <h4>{info.sla.deliveryTime} mins</h4>
-        <h4>User: { loggedInUser }</h4>
+      <div 
+      data-testid="resCard" 
+      className="mt-4 md:mt-0 p-4 w-65 min-h-80 border border-gray-300 flex flex-col md:m-4 bg-white hover:scale-105 transition ease-out shadow-md rounded-lg justify-center items-center">
+        <img 
+        className="-mt-1.5 rounded-lg h-40 w-full object-cover" 
+        alt="res-logo" 
+        src={CDN_URL + info.cloudinaryImageId}
+        />
+        <div className="flex flex-col grow">
+        <h4 className="font-bold py-2 text-md justify-center items-center flex text-sm -mt-1 m-0 text-center">{info.name}</h4>
+        <h4 className="text-black text-xs mx-auto -mt-1 text-center items-center">{info.cuisines.join(", ")}</h4>
+        <h4 className="mx-auto text-black text-xs mt-1">{info.avgRating}⭐</h4>
+        <h4 className="text-xs text-black mx-auto mt-1">{info.costForTwo}</h4>
+        <h4 className="text-xs text-black mx-auto mt-1">{info.sla.deliveryTime} mins</h4>
+        </div>
 
-      </div>
+        {/* <h4>User: { loggedInUser }</h4> */}
+        </div>
+    
     </Link>
   );
 };
@@ -50,8 +59,10 @@ export const OpenOrNot = (RestaurantCard:any) => {
 
   return (props:any) => {
     return (
-      <div>
-        <label className="absolute bg-black text-white m-2 p-2 rounded-lg">Open</label>
+      <div className="relative">
+        <label className="absolute top-6 left-6 z-20 bg-black text-white m-2 px-3 py-1 rounded-md text-xs font-semibold shadow-md pointer-cursor">
+          Open
+        </label>
       <RestaurantCard {...props} />
       </div>
     )
