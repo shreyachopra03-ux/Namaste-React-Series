c29879d4c74402ccbfa8d01bcb4fc7e4d88010af
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-28 00:20:39 +0530

    chore: update global type declarations

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,7 +1,11 @@
 declare module "*.css";
 
 declare module "*.svg" {
-  const value: string;
-  export default value;
+    const value: string;
+    export default value;
 }
 
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.webp";
