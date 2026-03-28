3aeef0ee6379a5430d14c5d4dcc0d5c62e80ba13
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-28 19:47:59 +0530

    style: improvements in accordion container

--- src/components/RestaurantCategory.tsx
+++ src/components/RestaurantCategory.tsx
@@ -14,16 +14,27 @@ const RestaurantCategory= ({ data, showItems, setShowIndex }:Props) => {
     }
 
     return (
-         <div>
-            {/* Accordion Header */}
-        <div className="mx-auto w-[95%] md:w-8/12 bg-gray-50 shadow-lg p-4 my-4">
-        <div className="flex justify-between cursor-pointer" onClick={handleClick}>
-            <span className="font-semibold text-md">{data?.title} ({data?.itemCards?.length})</span>
-            <span className="">⬇️</span>
+        <div className="w-full flex justify-center">
+        {/* Accordion container */}
+        <div className="w-full bg-white mx-auto md:w-8/12 shadow-sm p-4 my-4 border-b-8 border-gray-100 last:border-b-0">
+        {/* Accordion Header */}
+        <div 
+        className="flex justify-between cursor-pointer items-center p-4 hover:bg-gray-50 transition-all" 
+        onClick={handleClick}
+        >
+            <span className="font-bold font-sans text-gray-800 text-sm md:text-lg">
+                {data?.title} ({data?.itemCards?.length})
+            </span>
+            <span className={`transition-transform duration-300 ${showItems ? "rotate-180" : ""}`}>
+               ⬇️
+            </span>
         </div>
             
             {/* Accordion Body */}
+            <div className="overflow-hidden">
             {showItems && <ListItem items={data?.itemCards || []} />}
+            </div>
+            
         </div>
          </div>
 
